5f5521918028a3d15f474a2f4d522b981b497e6b
Elyaquim Nattan <elyaquimnattan@gmail.com>
2023-09-10 13:23:49 -0300

    commit final da primeira versão

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ const sign_in = document.querySelector('.Sign-in-button-title');
 const sign_up = document.querySelector('.Sign-up-button-title');
 const form = document.querySelector('.formulario');
 const divNome = document.querySelector('.Nome-div');
-const divEmail = document.querySelector('Email-div');
+const botao = document.querySelector('.sign-in-now-button')
 const termos = document.querySelector('.checkbox-and-a-div');
 
 document.addEventListener('click', function(e) {
     const el = e.target
 
-    //Ao clicar em Sign-In
+    //Ao clicar em Sign-Up
 
     if(el.classList.contains('Sign-up-button-title') || el.classList.contains('sign-up-button')) {
 
@@ -21,12 +21,13 @@ document.addEventListener('click', function(e) {
         sign_up.style.borderBottom = '1px solid #7d83ff';
         sign_in.style.color = 'rgba(255, 255, 255, 0.418)';
         sign_in.style.borderBottom = 'none'
-        
+        botao.innerHTML = 'Cadastre-se';
+
         form.insertBefore(divNome, form.firstChild);
         form.appendChild(termos);
     } 
     
-    //Ao clicar em Sign-Up
+    //Ao clicar em Sign-in
 
     else if (el.classList.contains('Sign-in-button-title') || el.classList.contains('sign-in-button')) {
 
@@ -36,6 +37,7 @@ document.addEventListener('click', function(e) {
         sign_in.style.borderBottom = '1px solid #7d83ff';
         sign_up.style.color = 'rgba(255, 255, 255, 0.418)';
         sign_up.style.borderBottom = 'none';
+        botao.innerHTML = 'Login';
 
         form.removeChild(divNome);
         form.removeChild(termos);
---
+++ readme.md
@@ -0,0 +1,23 @@
+<h1> 
+<img src="imagens/tela-login-e-cadastro-preview.jpeg">
+<p>Tela de Login e Cadastro</p>
+</h1>
+
+## 🚨 Sobre
+<br>
+
+**Tela de Login e Cadastro** é um projeto que desenvolvi para treinar e aprimorar minhas habilidades em Front-end, as ideias de design e criação foram desenvolvidas por mim mesmo. Adorei construir esse projeto que apesar de ser simples me ajudou bastante a me aprofundar nas técnologias.
+<br> <br>
+
+## 🔧 Ferramentas
+
+- HTML
+- CSS
+- JAVASCRIPT
+
+## **Versão: 1.0**
+
+<br>
+
+## 🎯 Link do projeto:
+### <a target="_blank" href="https://nattancode07.github.io/tela-de-login-cadastro/">https://nattancode07.github.io/tela-de-login-cadastro/</a>
